d3fd4f0b1972050027bfc03d1a1e7b373fae0a35
Station9586 <Louis072836@gmail.com>
2024-05-11 22:04:51 +0800

    Upload Problem A

---
+++ ABC353(0511)/a.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(v) ((int) (v).size())
+#define FOR(i, a, b) for (int i = a; i <= b; i ++)
+#define REP(i, b) for (int i = 0; i < b; i ++)
+#define IFOR(i, a, b) for (int i = b - 1; i >= a; i --) 
+
+typedef long long ll;
+typedef std::vector<int> vi;
+typedef std::pair<int, int> pi;
+
+template<class T, class U> std::istream& operator >> (std::istream& in, std::pair<T, U>& p) { in >> p.first >> p.second; return in; }
+template<class T, size_t N> std::istream& operator >> (std::istream& in, std::array<T, N>& a) { for (T &x: a) in >> x; return in; }
+template<class T> std::istream& operator >> (std::istream& in, std::vector<T>& a) { for (T &x: a) in >> x; return in; }
+template<class T> std::ostream& operator << (std::ostream& out, std::vector<T>& a) { for (T x: a) out << x << " "; return out; }
+
+using namespace std;
+
+
+int n;
+int main (int argc, const char *argv[]) {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin >> n;
+    vi v(n);
+    cin >> v;
+    int now = v[0];
+    int ans = -1;
+    FOR (i, 1, n - 1) {
+        if (v[i] > now) {
+            ans = i + 1;
+            break;
+        }
+    }
+    cout << ans << "\n";
+    return 0;
+}
